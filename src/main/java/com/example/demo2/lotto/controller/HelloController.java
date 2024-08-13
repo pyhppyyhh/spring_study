3e25c0205ca4838d73e8213697ce9e676ffb3d5f
pyhppyyhh <pyhppyyhh@naver.com>
2024-08-13 22:31:38 +0900

    프로젝트 등록

--- src/main/java/com/example/demo2/lotto/controller/HelloController.java
+++ src/main/java/com/example/demo2/lotto/controller/HelloController.java
@@ -3,11 +3,8 @@ package com.example.demo2.lotto.controller;
 
 
 import com.example.demo2.lotto.LottoGetApi;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.MalformedURLException;
